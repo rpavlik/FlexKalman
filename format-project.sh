e4b54cd1f9aa9d18745d0a7753e58006b110750b
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-10-29 12:09:42 -0500

    Clean up script

--- format-project.sh
+++ format-project.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-# Copyright 2019, Collabora, Ltd.
+# Copyright 2019, 2021, Collabora, Ltd.
 # SPDX-License-Identifier: BSL-1.0
+set -e
 (
-    cd $(dirname $0)
-    find inc src tests apps \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -i -style=file \{\} +
+    cd "$(dirname $0)"
+    find inc tests \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -i -style=file \{\} +
 )
